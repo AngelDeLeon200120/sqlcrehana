d5a0261ca6124ef6164d81c4330a1112a255214a
AngelCode2001 <angel.deleon@code.com.gt>
2024-09-02 16:26:12 -0600

    SE ACTUALIZO SELECT

--- SELECT.sql
+++ SELECT.sql
@@ -5,3 +5,19 @@ DESCRIBE Mascotas;
 SELECT * FROM  productos;
 
 SELECT * FROM productos WHERE tipo LIKE "%Casual%" AND precio BETWEEN 30 AND 50;
+
+SELECT * FROM productos order by PRECIO asc;
+
+SELECT * FROM proveedores;
+
+SELECT count(ID) AS numProveedores from proveedores;
+
+SELECT * FROM compras;
+
+SELECT count(nombre) AS Nombres_deCamisas FROM productos;
+
+SELECT FECHA AS dia,
+sum(importe) as importe
+FROM compras
+group by FECHA
+order by FECHA DESC;
